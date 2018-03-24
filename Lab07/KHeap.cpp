294c5c714fd9aa78e25834a23d5711faef5896a7
Adam Mertz <adammertz@gmail.com>
2018-03-24 10:21:54 -0500

    Implemented KHeap constructor/destructor

--- Lab07/KHeap.cpp
+++ Lab07/KHeap.cpp
@@ -1,1 +1,10 @@
 #include "KHeap.hpp"
+
+KHeap::KHeap(int k) {
+    this->_heap = new int[MAX_HEAP_SIZE]();
+    this->_k = k;
+    this->_size = 0;
+}
+
+KHeap::~KHeap() { delete[] this->_heap; }
+
