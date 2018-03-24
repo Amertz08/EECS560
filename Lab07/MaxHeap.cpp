556fbeb7eeb4b7cb844652b086ab0a863134d884
Adam Mertz <adammertz@gmail.com>
2018-03-24 10:13:22 -0500

    Added compare helper. Fixed DeleteMin & DeleteMax.

--- Lab07/MaxHeap.cpp
+++ Lab07/MaxHeap.cpp
@@ -10,6 +10,10 @@ MaxHeap::MaxHeap() {
 
 MaxHeap::~MaxHeap() { delete this->_heap; }
 
+bool MaxHeap::_compare(int parent, int child) {
+    return parent < child;
+}
+
 int MaxHeap::_parentIndex(int index) { return int(floor((index - 1) / this->_k)); }
 
 void MaxHeap::_printNode(int index) {
@@ -87,7 +91,7 @@ void MaxHeap::_upHeap(int index) {
     auto parentValue = this->_heap[parentIndex];
 
     // Check if we need to swap
-    if (parentValue < targetValue) {
+    if (this->_compare(parentValue, targetValue)) {
         // Swap
         this->_heap[parentIndex] = targetValue;
         this->_heap[index] = parentValue;
@@ -147,21 +151,13 @@ void MaxHeap::DeleteMin() {
         return;
     } else {
         // Root has children
-        auto minIndex = this->_minIndex() + 1;
-        auto min = this->_heap[minIndex];
+        auto minIndex = this->_minIndex();
 
-        // Look through children and find new min
-        for (int i = minIndex + 1; i < this->_k; i++) {
-            if (this->_heap[i] < min) {
-                min = this->_heap[i];
-                minIndex = i;
-            }
-        }
-
-        // Reassign root to new min
-        this->_heap[0] = min;
-
-        // Shift remaining values left
+        /*
+         * Min value in max heap would have to be in a leaf.
+         * Because of this we can simply shift all values after
+         * it to the left.
+         */
         for (int i = minIndex; i < this->_size; i++) {
             this->_heap[i] = this->_heap[i + 1];
         }
@@ -184,11 +180,15 @@ void MaxHeap::DeleteMax() {
 
         // Look through children and find new max
         for (int i = maxIndex + 1; i < this->_k; i++) {
+            std::cout << "index: " << i << std::endl;
             if (this->_heap[i] > max) {
                 max = this->_heap[i];
                 maxIndex = i;
             }
         }
+
+        // Reassign max to root
+        this->_heap[0] = max;
 
         // Shift remaining values left
         for (int i = maxIndex; i < this->_size; i++) {
