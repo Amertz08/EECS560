733cdc387eaf8f208e58110a0b89498cf68dd71c
Adam Mertz <adammertz@gmail.com>
2018-03-25 11:34:45 -0500

    Added function to check min/max of heap.

--- Lab07/test.cpp
+++ Lab07/test.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <assert.h>
 
 #include "MinHeap.hpp"
 #include "MaxHeap.hpp"
@@ -9,27 +10,53 @@ void printMinMax(KHeap* heap) {
     std::cout << "Min: " << heap->FindMin() << " Max: " << heap->FindMax() << std::endl;
 }
 
+void checkMinMax(KHeap* heap, int min, int max) {
+    auto hmin = heap->FindMin();
+    auto hmax = heap->FindMax();
+    std::cout << "Expecting min: " << min << " Heap min: " << hmin << std::endl;
+    assert(heap->FindMin() == min);
+    std::cout << "Expecting max: " << max << " Heap max: " << hmax << std::endl;
+    assert(heap->FindMax() == max);
+}
+
 void test(KHeap* heap) {
     heap->Print();
     heap->LevelOrder();
     printMinMax(heap);
-    std::cout << "Calling BuildHeap\n";
+    std::cout << "TEST: Calling BuildHeap\n";
     heap->BuildHeap();
     heap->Print();
     heap->LevelOrder();
     printMinMax(heap);
+    checkMinMax(heap, 1, 402);
 
-    std::cout << "Calling DeleteMin\n";
+    std::cout << "TEST: Calling DeleteMin\n";
     heap->DeleteMin();
     heap->Print();
     heap->LevelOrder();
     printMinMax(heap);
+    checkMinMax(heap, 11, 402);
 
-    std::cout << "Calling DeleteMax\n";
+    std::cout << "TEST: Calling DeleteMax\n";
     heap->DeleteMax();
     heap->Print();
     heap->LevelOrder();
     printMinMax(heap);
+    checkMinMax(heap, 11, 317);
+
+    std::cout << "TEST: Inserting 5\n";
+    heap->Insert(5);
+    heap->Print();
+    heap->LevelOrder();
+    printMinMax(heap);
+    checkMinMax(heap, 5, 317);
+
+    std::cout << "TEST: Inserting 500\n";
+    heap->Insert(500);
+    heap->Print();
+    heap->LevelOrder();
+    printMinMax(heap);
+    checkMinMax(heap, 5, 500);
 }
 
 int main(int argc, char* argv[]) {
@@ -40,7 +67,6 @@ int main(int argc, char* argv[]) {
 
     std::ifstream file;
     std::string fileName = argv[1];
-    std::cout << fileName << std::endl;
 
     MaxHeap max(5);
     MinHeap min(5);
