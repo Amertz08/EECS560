5261d389bfa813dccadcaabfe1173e9f9121c7a0
Adam Mertz <adammertz@gmail.com>
2018-04-22 19:23:07 -0500

    Refactored testing into common function. Added Skew heap to testing

--- Lab10/test.cpp
+++ Lab10/test.cpp
@@ -1,46 +1,60 @@
 #include <iostream>
 
+#include "Heap.hpp"
 #include "Leftist.hpp"
+#include "Skew.hpp"
+
+void TestTree(Heap* h, const std::string &type)
+{
+    std::cout << "TESTING " << type << std::endl;
+    h->Insert(5);
+    h->PreOrder();
+    h->InOrder();
+    h->Insert(2);
+    h->PreOrder();
+    h->InOrder();
+    h->Insert(7);
+    h->PreOrder();
+    h->InOrder();
+    h->Insert(10);
+    h->PreOrder();
+    h->InOrder();
+    h->Insert(6);
+    h->PreOrder();
+    h->InOrder();
+    h->Insert(8);
+    h->PreOrder();
+    h->InOrder();
+    h->Insert(3);
+    h->PreOrder();
+    h->InOrder();
+    h->Insert(6);
+    h->PreOrder();
+    h->InOrder();
+    h->Insert(2);
+    h->PreOrder();
+    h->InOrder();
+    std::cout << "Done inserting\n";
+
+    std::cout << "DeleteMin\n";
+    h->DeleteMin();
+    h->PreOrder();
+    h->InOrder();
+    std::cout << "DeleteMin\n";
+    h->DeleteMin();
+    h->PreOrder();
+    h->InOrder();
+
+    auto min = h->FindMin();
+    std::cout << "Min: " << min << std::endl;
+    std::cout << "Exiting\n";
+}
 
 int main(int argc, char* argv[]) {
     Leftist lh;
+    Skew sk;
     std::cout << "STARTING TEST PROGRAM\n";
 
-    lh.Insert(5);
-    lh.PreOrder();
-    lh.InOrder();
-    lh.Insert(2);
-    lh.PreOrder();
-    lh.InOrder();
-    lh.Insert(7);
-    lh.PreOrder();
-    lh.InOrder();
-    lh.Insert(10);
-    lh.PreOrder();
-    lh.InOrder();
-    lh.Insert(6);
-    lh.PreOrder();
-    lh.InOrder();
-    lh.Insert(8);
-    lh.PreOrder();
-    lh.InOrder();
-    lh.Insert(3);
-    lh.PreOrder();
-    lh.InOrder();
-    lh.Insert(6);
-    lh.PreOrder();
-    lh.InOrder();
-    lh.Insert(2);
-    lh.PreOrder();
-    lh.InOrder();
-    std::cout << "Done inserting\n";
-
-    lh.DeleteMin();
-    lh.PreOrder();
-    lh.DeleteMin();
-    lh.PreOrder();
-
-    auto min = lh.FindMin();
-    std::cout << "Min: " << min << std::endl;
-    std::cout << "Exiting\n";
+    TestTree(&lh, "Leftist");
+    TestTree(&sk, "Skew");
 }
